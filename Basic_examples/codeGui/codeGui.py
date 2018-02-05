432a41513ad3db56b351fcb5617c4233a8bcb600
Akhil2453 <akhil.menon94@gmail.com>
2018-02-05 14:36:56 +0530

    added window icon to file codeGui

--- Basic_examples/codeGui/codeGui.py
+++ Basic_examples/codeGui/codeGui.py
@@ -6,6 +6,7 @@ window = QtWidgets.QWidget()			#create an object of class QWidget for window
 
 window.setGeometry(50,50,500,300)		#window start position on the screen, and total window size
 window.setWindowTitle("First GUI")		#set window title
+window.setWindowIcon(QtGui.QIcon('index.jpg'))
 
 window.show()							#show the window created
 sys.exit(app.exec_())					#exit the window on pressing the close button
