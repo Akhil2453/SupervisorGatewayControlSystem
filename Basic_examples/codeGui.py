4a8002c7e0cac4eb384e97821e1706ffe79a7bec
Akhil2453 <akhil.menon94@gmail.com>
2018-02-05 09:06:44 +0530

    small gui using only pythin script

---
+++ Basic_examples/codeGui.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+app = QtWidgets.QApplication(sys.argv)  #crete an object of class QApplication
+window = QtWidgets.QWidget()			#create an object of class QWidget for window
+
+window.setGeometry(50,50,500,300)		#window start position on the screen, and total window size
+window.setWindowTitle("First GUI")		#set window title
+
+window.show()							#show the window created
+sys.exit(app.exec_())					#exit the window on pressing the close button
