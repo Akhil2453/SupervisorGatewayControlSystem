3afd2567a40bf7949e38ad6cd01358e3300b0fa7
Akhil2453 <akhil.menon94@gmail.com>
2018-02-05 10:21:17 +0530

    added quit button

--- Basic_examples/appClass.py
+++ Basic_examples/appClass.py
@@ -8,9 +8,19 @@ class Window(QtWidgets.QWidget):
 		self.setGeometry(50,50,500,500)
 		self.setWindowTitle("Akhil")
 		self.setWindowIcon(QtGui.QIcon('index.jpg'))
+		self.home()
+
+	def home(self):
+		btn = QtWidgets.QPushButton("quit", self)
+		btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+		btn.resize(100,50)
+		btn.move(200,200)
 		self.show()
 
+def run():
+	app = QtWidgets.QApplication(sys.argv)
+	GUI = Window()
+	sys.exit(app.exec_())
 
-app = QtWidgets.QApplication(sys.argv)
-GUI = Window()
-sys.exit(app.exec_())
+
+run()
